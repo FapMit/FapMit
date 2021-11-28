c96e4945e580016964609e3dabf0264968c40415
Maksim <fapmit01@gmail.com>
2021-11-28 17:59:06 +0300

    commit №2

--- ABD.py
+++ ABD.py
@@ -20,6 +20,5 @@ while True:
         k = (-1)/(4*(n**2)+6*n+2)
         sum = k*sum
         function += sum
-        n += 1
-        
+        n += 1        
 print('Значение правой части уравнения: ', function)        
